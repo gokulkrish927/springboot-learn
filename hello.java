f0ee758ceda74920d1cc1c6f9a678221301cd2a8
Gokula Krishnan <gokulkrish927@gmail.com>
2022-07-27 17:39:55 +0530

    3 commit

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String args[]) {
         System.out.printf("hello");
+        System.out.printf("hello");
+        System.out.printf("hello");
+        System.out.printf("hello");
     }
 }
